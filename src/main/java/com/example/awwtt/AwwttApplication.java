7819f14bd0cc6c73ba679462910c472309bf8029
Michał Macias <michal.maciasxd@gmail.com>
2023-11-13 22:42:01 +0100

    init commit

---
+++ src/main/java/com/example/awwtt/AwwttApplication.java
@@ -0,0 +1,13 @@
+package com.example.awwtt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwwttApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwwttApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/awwtt/AwwttApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.awwtt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwwttApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
